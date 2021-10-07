245485edd021bbb276f1f53660c7aa0f55ad8c17
Bers <mbers.pro@gmail.com>
2021-10-07 14:19:11 +0300

    Mistake

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ImageGallery from "./imageGallery/ImageGallery";
 import Searchbar from "./searchbar/Searchbar";
 import { getApiData } from "../utils/Api";
-import Button from "./button/Button";
 
 class App extends Component {
   state = {
@@ -45,12 +44,12 @@ class App extends Component {
       <>
         <Searchbar onSubmit={this.onSubmit} />
         <ImageGallery imgList={this.state.imgList} />
-        {this.state.imgList.length > 0 && (
-          <Button onLoadMore={this.onLoadMore} />
-        )}
+        
       </>
     );
   }
 }
 
 export default App;
+
+
--- src/components/imageGallery/ImageGallery.js
+++ src/components/imageGallery/ImageGallery.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import ImageGalleryItem from "./imageGalleryItem/ImageGalleryItem";
 import { imageGallery } from "./ImageGallery.module.css";
+import Button from "../button/Button";
 
 class ImageGallery extends Component {
   state = {};
 
   render() {
     return (
-      <ul className={imageGallery}>
-        {this.props.imgList.map((item) => (
-          <ImageGalleryItem img={item} />
-        ))}
-      </ul>
+      <>
+        <ul className={imageGallery}>
+          {this.props.imgList.map((item) => (
+            <ImageGalleryItem img={item} />
+          ))}
+        </ul>
+        {this.props.imgList.length > 0 && (
+          <Button onLoadMore={this.onLoadMore} />
+        )}
+      </>
     );
   }
 }
